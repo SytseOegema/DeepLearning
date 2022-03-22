c6388630364a67ecc6ab2cffd9e6c0ab2440fe51
Sytse Oegema <sy.oegema@oegematransport.nl>
2022-03-22 11:24:54 +0100

    GPU script

--- jobscript.sh
+++ jobscript.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --time=00:04:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8gb
 #SBATCH --job-name=python_train_BERT
-#SBATCH --mem=2gb
 
 module purge
 module load TensorFlow/2.5.0-fosscuda-2020b
 
 pip install -r code/requirements.txt --user
 
-python code/main.py
+mpirun python code/main.py
 
 deactivate
