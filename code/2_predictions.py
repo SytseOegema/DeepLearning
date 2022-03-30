bc5321cf858a177c31eca9c78d0a2160dab62455
Sytse Oegema <sy.oegema@oegematransport.nl>
2022-03-30 13:38:45 +0200

    Refactorings

---
+++ code/2_predictions.py
@@ -0,0 +1,24 @@
+from helpers.store_results import create_specific_folder
+from helpers.predict import predict
+
+pre_trained_paths = [
+    "../pre_trained_models/from_scratch_5_epoch/",
+    "../pre_trained_models/from_scratch_10_epoch/",
+    "../pre_trained_models/pre_trained_plus_2_epoch",
+    "../pre_trained_models/pre_trained_plus_5_epoch"
+]
+
+storage_paths = [
+    "../data/from_scratch_5_epoch/",
+    "../data/from_scratch_10_epoch/",
+    "../data/pre_trained_plus_2_epoch",
+    "../data/pre_trained_plus_5_epoch"
+]
+
+data_base_path = "/data/squat.dat"
+
+for idx in range(4):
+    create_specific_folder(storage_paths[idx])
+    print("pre_trained_path: " + pre_trained_paths[idx])
+    print("storage_path: " + storage_paths[idx])
+    print("data_base_path: " + data_base_path)
